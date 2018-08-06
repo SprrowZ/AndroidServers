5e86a05ae05c97cc0dc63d873a3f73d691ae56d5
SprrowZ <33713300+SprrowZ@users.noreply.github.com>
2018-08-06 21:14:14 +0800

    提交

--- src/imooc/SocketServer.java
+++ src/imooc/SocketServer.java
@@ -1,7 +1,10 @@
 package imooc;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -15,9 +18,10 @@ public class SocketServer {
 
 	public static void startServer() {
 		// TODO Auto-generated method stub
-		ServerSocket serverSocket;
-		BufferedReader reader;
-		Socket socket;
+		ServerSocket serverSocket=null;
+		BufferedReader reader=null;
+		Socket socket=null;
+		BufferedWriter writer=null;
 		try {
 
 			serverSocket = new ServerSocket(20133);// 新建一个socket对象
@@ -25,14 +29,26 @@ public class SocketServer {
 			socket = serverSocket.accept();//等待客户端输入，一直阻塞状态
 			System.out.println("socket开始运行...");
 			reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+			writer=new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
 			String receivedMsg;
 			while ((receivedMsg = reader.readLine()) != null) {
 				System.out.println(receivedMsg);
+				writer.write("receivedMsg:"+receivedMsg+"\n");
+				writer.flush();
 			}
 		} catch (Exception e) {
            e.printStackTrace();
 		}finally {
-			
+			try {
+				reader.close();
+				writer.close();
+				socket.close();
+				serverSocket.close();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		
 		}
 	}
 
